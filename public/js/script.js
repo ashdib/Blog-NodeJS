c77a7c22ef10f13069ad276dbd9b748b1e53dc59
axeladib <nabil298adib@gmail.com>
2024-03-23 12:11:34 +0800

    Making the animation for searchBar to open and close in script.js

--- public/js/script.js
+++ public/js/script.js
@@ -4,5 +4,29 @@
 document.addEventListener("DOMContentLoaded", function () {
   // select the searchButton in ./partials/header.ejs
   const allButtons = document.querySelectorAll(".searchBtn");
-  const searchBar = document.querySelectorAll(".searchBtn");
+  // select the searchBar in ./partials/search.ejs
+  const searchBar = document.querySelector(".searchBar");
+  // select the searchInput ID in ./partials/search.ejs
+  const searchInput = document.getElementById("searchInput");
+  // select the searchClose ID in ./partials/search.ejs
+  const searchClose = document.getElementById("searchClose");
+
+  // TODO: Making the searchButton when click the searchBar will poopup
+  for (var i = 0; i < allButtons.length; i++) {
+    allButtons[i].addEventListener("click", function () {
+      searchBar.style.visibility = "visible";
+      searchBar.classList.add("open");
+      // Making it expanded
+      this.setAttribute("aria-expanded", true);
+      searchInput.focus();
+    });
+  }
+
+  // TODO: Making the search fill close when click the "close"
+  searchClose.addEventListener("click", function () {
+    searchBar.style.visibility = "hidden";
+    searchBar.classList.remove("open");
+    // Making it expanded
+    this.setAttribute("aria-expanded", false);
+  });
 });
