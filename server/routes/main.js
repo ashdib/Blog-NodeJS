4a821e5414856ef719b352fa0a199379795ed096
axeladib <nabil298adib@gmail.com>
2024-03-23 09:18:31 +0800

    adding dynamic routes for each post to have individual page

--- server/routes/main.js
+++ server/routes/main.js
@@ -10,9 +10,40 @@ router.get("", async (req, res) => {
   };
 
   try {
-    //TODO: Display the list of posts from the MongoDB
-    const data = await Post.find();
-    res.render("index.ejs", { locals, data });
+    // TODO: Create pagination for the posts
+    let perPage = 6;
+    let page = req.query.page || 1;
+    //                                   sort the posts by createdAt in descending order
+    /**
+     * Represents the aggregated data obtained from the database.
+     * @typedef {Object[]} AggregatedData
+     * @property {number} createdAt - The creation timestamp of the post.
+     * @property {string} // add more properties as needed
+     */
+
+    /**
+     * Retrieves aggregated data from the database.
+     * @returns {Promise<AggregatedData>} The aggregated data.
+     */
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      // skip over the post that come before the current page. Eg: if the user navigate to page 3, it will skip the first 20 posts
+      .skip(perPage * page - perPage)
+      // limit the number of documents returned to the number of posts per page.
+      .limit(perPage)
+      // executes the aggregation pipeline and returns a promise that resolves to the result data
+      .exec();
+    // to count the number/total number of posts in the database
+    const count = await Post.countDocuments();
+    // calculate the number for the next page
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index.ejs", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
   } catch (error) {
     console.log(error);
   }
@@ -21,6 +52,42 @@ router.get("", async (req, res) => {
 router.get("/about", (req, res) => {
   res.render("about.ejs");
 });
+
+/**
+ * Get /
+ * Post : id **/
+// TODO: Connect every post's article to a page contain the content/body of the post of the post
+router.get("/post/:id", async (req, res) => {
+  try {
+    const slug = req.params.id;
+    const data = await Post.findById({ _id: slug });
+    const locals = {
+      title: data.title,
+      description:
+        "Simple blog web application with NodeJS, ExpressJS and MongoDB.",
+    };
+    res.render("post.ejs", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// TODO: Test out to retrieve data from the MongoDB
+// router.get("", async (req, res) => {
+//   const locals = {
+//     title: "NodeJS Blog",
+//     description:
+//       "Simple blog web application with NodeJS, ExpressJS and MongoDB.",
+//   };
+
+//   try {
+//     //TODO: Display the list of posts from the MongoDB
+//     const data = await Post.find();
+//     res.render("index.ejs", { locals, data });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 // TODO: Test out the Post model & adding data to the MongoDB
 // Create a function to insert title and body data to the MongoDB
